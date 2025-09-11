0cda5d09418bcac0a00cd260494fec0637772cb3
farzeenilyaszargar <farzeenilyaszargar@gmail.com>
2025-09-11 10:57:46 +0530

    main start

---
+++ README.md
@@ -0,0 +1,20 @@
+# Conway's Game of Life
+
+
+It is a cellular automaton devised by John Conway in 1970. Here evolution is determined by the initial state, requiring no input making it a 0 player game. It is turing complete and can simulate a universal constructor or any other turing machine.
+
+*I made this game in C to explore more C libraries and was interested in studying cellular automaton suggested by Stanislav Ulam and Von Neumann. Also their model of fluid flow was too difficult to make due to each cell having 27 different states* 
+
+<div style="text-align:center;">
+<img src="https://upload.wikimedia.org/wikipedia/commons/e/e5/Gospers_glider_gun.gif">
+</div>
+
+
+#### Rules Withn The Cells
+The universe of the Game of Life is an infinite, two-dimensional orthogonal grid of square cells, each of which is in one of two possible states, _live_ or _dead_ (or _populated_ and _unpopulated_, respectively). Every cell interacts with its eight neighbours, which are the cells that are horizontally, vertically, or diagonally adjacent. At each step in time, the following transitions occur:
+1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+2. Any live cell with two or three live neighbours lives on to the next generation.
+3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+
--- lol.cpp
+++ lol.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <SDL.h>
+// #include <SDL_ttf.h>
 
 using namespace std;
 
-const int WINDOW_HEIGHT = 600;
+const int WINDOW_HEIGHT = 670;
 const int WINDOW_WIDTH = 600;
+const int TOPBAR_HEIGHT = 70;
 const int ROWS = 20;
 const int COLS = 20;
 const int CELL_W = WINDOW_WIDTH/COLS;
-const int CELL_H = WINDOW_HEIGHT/ROWS;
+const int CELL_H = (WINDOW_HEIGHT-TOPBAR_HEIGHT)/ROWS;
 
 
 int main()
@@ -19,7 +21,7 @@ int main()
         return 1;
     }
 
-    SDL_Window* window = SDL_CreateWindow("Conways Game of Life By Fizzy", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_HEIGHT, WINDOW_WIDTH, SDL_WINDOW_SHOWN);
+    SDL_Window* window = SDL_CreateWindow("Conways Game of Life By Fizzy", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
     
     SDL_Event event;
@@ -27,6 +29,13 @@ int main()
     int running = 1;
 
     bool active[ROWS][COLS] = {false};
+    bool startPlay = false;
+
+    SDL_Rect startBtn = {100, 10, 100, 50};
+    SDL_Rect stopBtn = {250, 10, 100, 50};
+    SDL_Rect resetBtn = {400, 10, 100, 50};
+
+
 
     while (running)
     {
@@ -39,22 +48,62 @@ int main()
                 int mouseX = event.button.x;
                 int mouseY = event.button.y;
 
-                int col = mouseX/CELL_W;
-                int row = mouseY/CELL_H;
+                SDL_Point startPoints = {mouseX, mouseY};
+                cout << "Mouse clicked at: " << mouseX << ", " << mouseY << endl;
 
-                active[row][col] = !active[row][col];
+
+                if (SDL_PointInRect(&startPoints, &startBtn))
+                {
+                    cout << "Start Button Pressed";
+                }
+
+                if (SDL_PointInRect(&startPoints, &stopBtn))
+                {
+                    cout << "Stop Button Pressed";
+                }
+
+                 if (SDL_PointInRect(&startPoints, &resetBtn))
+                {
+                    cout << "Reset Button Pressed";
+                }
+
+                if (mouseY>TOPBAR_HEIGHT)
+                {
+                    int col = mouseX/CELL_W;
+                    int row = (mouseY-TOPBAR_HEIGHT)/CELL_H;
+                    active[row][col] = !active[row][col];
+
+                }
+
+
+
+                
             }
+            
             
         }
 
         SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
         SDL_RenderClear(renderer);
 
+        SDL_SetRenderDrawColor(renderer, 0, 0, 200, 255);
+        SDL_RenderFillRect(renderer, &startBtn);
+
+        SDL_SetRenderDrawColor(renderer, 200, 0, 0, 255);
+        SDL_RenderFillRect(renderer, &stopBtn);
+
+
+        SDL_SetRenderDrawColor(renderer, 0, 200, 0, 255);
+        SDL_RenderFillRect(renderer, &resetBtn);
+
+
+
+
         for (int r = 0; r<ROWS; r++)
         {
             for (int c = 0; c<COLS; c++)
             {
-                SDL_Rect cell = {c*CELL_W, r*CELL_H, CELL_W, CELL_H};
+                SDL_Rect cell = {c*CELL_W, TOPBAR_HEIGHT+(r*CELL_H), CELL_W, CELL_H};
                 if(active[r][c])
                 {
                     SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
